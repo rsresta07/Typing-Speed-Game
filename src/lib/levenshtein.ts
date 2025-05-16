f6669fc5aa3204027e4fcc3a07b95b7fe9137fd8
Rameshwor <r.sresta07@gmail.com>
2025-05-16 18:00:23 +0545

    [Add]: Levenshtein distance utility and class name merging function

---
+++ src/lib/levenshtein.ts
@@ -0,0 +1,17 @@
+// Utility for distance calc
+
+export function levenshtein(a: string, b: string): number {
+  const matrix = Array.from({ length: a.length + 1 }, (_, i) => [i]);
+  for (let j = 0; j <= b.length; j++) matrix[0][j] = j;
+  for (let i = 1; i <= a.length; i++) {
+    for (let j = 1; j <= b.length; j++) {
+      const cost = a[i - 1] === b[j - 1] ? 0 : 1;
+      matrix[i][j] = Math.min(
+        matrix[i - 1][j] + 1,
+        matrix[i][j - 1] + 1,
+        matrix[i - 1][j - 1] + cost
+      );
+    }
+  }
+  return matrix[a.length][b.length];
+}
